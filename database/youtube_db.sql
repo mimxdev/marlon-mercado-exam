cc6dee7fc032c16b2a541ed733c3d3e42dc02e06
Marlon Mercado <marlonilaganmercado@gmail.com>
2023-07-22 23:52:34 +0800

    added database file

---
+++ database/youtube_db.sql
@@ -0,0 +1,86 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 22, 2023 at 05:50 PM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 7.4.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `youtube_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `youtube_channels`
+--
+
+CREATE TABLE `youtube_channels` (
+  `id` int(11) NOT NULL,
+  `profile_picture` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(9000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `youtube_channel_videos`
+--
+
+CREATE TABLE `youtube_channel_videos` (
+  `id` int(11) NOT NULL,
+  `video_link` varchar(500) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` varchar(10000) NOT NULL,
+  `thumbnail` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `youtube_channels`
+--
+ALTER TABLE `youtube_channels`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `youtube_channel_videos`
+--
+ALTER TABLE `youtube_channel_videos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `youtube_channels`
+--
+ALTER TABLE `youtube_channels`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `youtube_channel_videos`
+--
+ALTER TABLE `youtube_channel_videos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
